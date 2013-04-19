e53f214e395cb752b630c2599b285371b218f546
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-04-19 19:11:13 +0000

    - maven: removing duplicated method that is implemented by the kernel also
    git-svn-id: https://efaps.googlecode.com/svn/maven-eFaps-PlugIn/trunk@9210 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/java/org/efaps/maven/plugin/EFapsAbstractMojo.java
+++ src/main/java/org/efaps/maven/plugin/EFapsAbstractMojo.java
@@ -20,12 +20,8 @@
 
 package org.efaps.maven.plugin;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.maven.plugin.Mojo;
 import org.apache.maven.plugin.logging.Log;
@@ -150,10 +146,10 @@ public abstract class EFapsAbstractMojo
                 AppAccessHandler.init(null, new HashSet<String>());
                 StartupDatabaseConnection.startup(this.type,
                                                   this.factory,
-                                                  convertToMap(this.connection),
+                                                  this.connection,
                                                   this.transactionManager,
                                                   this.transactionSynchronizationRegistry,
-                                                  convertToMap(this.configProps));
+                                                  this.configProps);
             }
         } catch (final StartupException e) {
             getLog().error("Initialize Database Connection failed: " + e.toString());
@@ -201,39 +197,6 @@ public abstract class EFapsAbstractMojo
         throws EFapsException
     {
         Context.commit();
-    }
-
-    /**
-     * Separates all key / value pairs of given text string.<br/>
-     * Evaluation algorithm:<br/>
-     * Separates the text by all found commas (only if in front of the comma is
-     * no back slash). This are the key / value pairs. A key / value pair is
-     * separated by the first equal ('=') sign.
-     *
-     * @param _text text string to convert to a key / value map
-     * @return Map of strings with all found key / value pairs
-     */
-    protected Map<String, String> convertToMap(final String _text)
-    {
-        final Map<String, String> properties = new HashMap<String, String>();
-
-        if (_text != null) {
-            // separated all key / value pairs
-            final Pattern pattern = Pattern.compile("(([^\\\\,])|(\\\\,)|(\\\\))*");
-            final Matcher matcher = pattern.matcher(_text);
-
-            while (matcher.find()) {
-                final String group = matcher.group().trim();
-                if (group.length() > 0) {
-                    // separated key from value
-                    final int index = group.indexOf('=');
-                    final String key = (index > 0) ? group.substring(0, index).trim() : group.trim();
-                    final String value = (index > 0) ? group.substring(index + 1).trim() : "";
-                    properties.put(key, value);
-                }
-            }
-        }
-        return properties;
     }
 
      /**
